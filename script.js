1bc69ffd01b07735a287fdb7e0467872afa5badb
Glitch (decisive-marimba) <none>
2018-03-16 13:52:38 +0000

    🎂😬 Checkpoint ./index.html:756894/12 ./script.js:756894/705

---
+++ script.js
@@ -0,0 +1,26 @@
+const cameraElement = document.querySelector("#avatar");
+
+let jumping = false;
+
+function jump() {
+  if (!jumping) {
+    jumping = true;
+    cameraElement.dispatchEvent(new CustomEvent('jump'));
+    setTimeout(() => {
+      jumping = false;
+    }, 500);
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === ' ') {
+    jump();
+  }
+});
+
+window.addEventListener('devicemotion', (e) => {
+  const accel = e.accelerationIncludingGravity;
+  
+  const accelerationThreshold = 20;
+  if (accel && accel.x > accelerationThreshold
+});
