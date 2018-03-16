b5400311024a649f2e3ebe76d62110dd4975fa43
Glitch (prickle-roll) <none>
2018-03-16 08:09:03 +0000

    🦃🌥 Checkpoint ./index.html:442513/4211 ./.glitch-assets:442513/764 ./script.js:442513/779

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('hi');
+const cameraElement = document.querySelector('#avatar');
+const remote = document.querySelector('#remote');
+const jumpAccelerationTreshold = 20;
+
+let jumping = false;
+
+function jump() {
+  if (!jumping) {
+    jumping = true;
+    cameraElement.dispatchEvent(new CustomEvent('jump'))
+    setTimeout(function(){
+      jumping = false;
+    }, 600)
+  }
+}
+
+document.addEventListener('keydown', (e) => {
+  if (e.key === ' ') {
+    jump();
+  }
+});
+
+window.addEventListener('devicemotion', (e) => {
+  const accel = e.accelerationIncludingGravity;
+  if (accel && accel.x > jumpAccelerationTreshold) {
+    jump();
+  }
+}, true);
